be61e4fce829045b4f3553cfa951985c8fb0f41a
anton.shadaev <anton.shadaev@nordclan.com>
2024-02-01 12:49:03 +0400

    Add browse files

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.nio.file.FileSystems;
 
 
 public class Main extends JFrame {
@@ -11,14 +14,21 @@ public class Main extends JFrame {
     private JTextField textFieldSecondCol;
     private JTextField textFieldThirdCol;
     private JLabel labelSelectedFile;
+    private JButton buttonParse;
 
     public Main() {
         buttonFileChooser.addActionListener(e -> {
             JFileChooser fileopen = new JFileChooser();
+            fileopen.setCurrentDirectory(new File(System.getProperty("user.home") + FileSystems.getDefault().getSeparator() + "Desktop"));
             int result = fileopen.showDialog(null, "Browse files");
             if (result == JFileChooser.APPROVE_OPTION) {
                 File file = fileopen.getSelectedFile();
                 labelSelectedFile.setText(file.getName());
+            }
+        });
+        buttonParse.addActionListener(e -> {
+            if (textFieldFirstCol.getText().isBlank() || textFieldSecondCol.getText().isBlank() || textFieldThirdCol.getText().isBlank()) {
+                JOptionPane.showMessageDialog(this, "Please, fill in the textfield!");
             }
         });
     }
