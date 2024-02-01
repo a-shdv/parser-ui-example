fe78d463e26c98d1495bbe79512cb246cfbd85c6
anton.shadaev <anton.shadaev@nordclan.com>
2024-02-01 12:37:35 +0400

    Add browse files

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,25 @@
 package com.company;
 
 import javax.swing.*;
+import java.io.File;
 
 
 public class Main extends JFrame {
     private JPanel panelMain;
     private JButton buttonFileChooser;
+    private JTextField textFieldFirstCol;
+    private JTextField textFieldSecondCol;
+    private JTextField textFieldThirdCol;
+    private JLabel labelSelectedFile;
 
     public Main() {
         buttonFileChooser.addActionListener(e -> {
+            JFileChooser fileopen = new JFileChooser();
+            int result = fileopen.showDialog(null, "Browse files");
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File file = fileopen.getSelectedFile();
+                labelSelectedFile.setText(file.getName());
+            }
         });
     }
 
@@ -16,6 +27,7 @@ public class Main extends JFrame {
         Main main = new Main();
         main.setContentPane(main.panelMain);
         main.pack();
+        main.setSize(800, 400);
         main.setVisible(true);
         main.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
